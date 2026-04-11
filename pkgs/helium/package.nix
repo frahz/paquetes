4e875246aa6c639cd1c3244887753e94a144acf6
frahz <me@frahz.dev>
2026-04-11 12:37:51 -0700

    helium: init at 0.11.5.1

---
+++ pkgs/helium/package.nix
@@ -0,0 +1,47 @@
+{
+  lib,
+  appimageTools,
+  fetchurl,
+  widevine-cdm,
+  nix-update-script,
+  ...
+}:
+appimageTools.wrapAppImage rec {
+  pname = "helium";
+  version = "0.11.5.1";
+
+  src = appimageTools.extract {
+    inherit pname version;
+    src = fetchurl {
+      url = "https://github.com/imputnet/helium-linux/releases/download/${version}/helium-${version}-x86_64.AppImage";
+      hash = "sha256-Ni7IZ9UBafr+ss0BcQaRKqmlmJI4IV1jRAJ8jhcodlg=";
+    };
+
+    postExtract = ''
+      mkdir -p $out/opt/helium
+      cp -a ${widevine-cdm}/share/google/chrome/WidevineCdm $out/opt/helium
+    '';
+  };
+
+  extraInstallCommands = ''
+    install -Dm444 {${src},$out/share/applications}/helium.desktop
+    install -Dm444 {${src}/usr,$out}/share/icons/hicolor/256x256/apps/helium.png
+  '';
+
+  # TODO: check if needed
+  extraBwrapArgs = [
+    "--ro-bind-try /etc/chromium/policies/managed/default.json /etc/chromium/policies/managed/default.json"
+    "--ro-bind-try /etc/xdg/ /etc/xdg/"
+  ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    description = "Helium Browser";
+    homepage = "https://github.com/imputnet/helium-linux";
+    downloadPage = "https://github.com/imputnet/helium-linux/releases";
+    mainProgram = "helium";
+    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
